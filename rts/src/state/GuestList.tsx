0383e5882c0d7a5e9d0322f10023e0cc700084c2
mairi tikk <mairi.tikk@gmail.com>
2024-09-23 10:19:22 +0200

    add new state

--- rts/src/state/GuestList.tsx
+++ rts/src/state/GuestList.tsx
@@ -1,14 +1,26 @@
-/* eslint-disable no-unreachable */
-
 import { useState } from 'react';
 
 const GuestList: React.FC = () => {
-    const [name, setName] = useState('');
-    return (<div>
-        <h3>Guest List</h3>
-        <input value={name} onChange= {(e) => setName(e.target.value)} type="text" />
-        <button>Add Guest</button>
+  const [name, setName] = useState('');
+  const [guests, setGuests] = useState<string[]>([]);
+
+  const onClick = () => {
+    setName('');
+    setGuests([...guests, name]);
+  };
+
+  return (
+    <div>
+      <h3>Guest List</h3>
+      <ul>
+        {guests.map((guest) => (
+          <li key={guest}>{guest}</li>
+        ))}
+      </ul>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={onClick}>Add Guest</button>
     </div>
-    );
+  );
 };
-export default GuestList; 
+
+export default GuestList;
