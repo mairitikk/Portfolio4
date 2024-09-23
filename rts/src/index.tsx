a896930f1ee13e2e703c7093eef33f203659f6d4
mairi tikk <mairi.tikk@gmail.com>
2024-09-23 10:32:34 +0200

    on State

--- rts/src/index.tsx
+++ rts/src/index.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from "react-dom";
 import GuestList from "./state/GuestList";
+import UserSearch from "./state/UserSearch";
+
 
 const App = () => {
     return <div>
         <h1>Hello, World!</h1>
         <GuestList></GuestList>
+        <UserSearch></UserSearch>
     </div>
 }
 ReactDOM.render(<App />, document.querySelector('#root'));
--- rts/src/state/UserSearch.tsx
+++ rts/src/state/UserSearch.tsx
@@ -1,7 +1,22 @@
 import { useState } from 'react';
 
+const users = [
+    { name: 'Mairi', age: 33 },
+    { name: 'Leena', age: 33},
+    { name: 'Liam', age: 33 }
+];
+
 const UserSearch: React.FC = () => {
-return <div>Otsi kasutajat</div>
+
+    const [name, setName] = useState('');
+
+    const onClick = () => {
+
+    }; 
+    return <div>Otsi kasutajat
+        <input value={name} onChange={e => setName(e.target.value)} type="text" />
+        <button onClick={onClick}>Otsi kasutajat</button>
+</div>
 };
 
 export default UserSearch;
