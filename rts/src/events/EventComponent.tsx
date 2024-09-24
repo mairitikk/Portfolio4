c9c3877457799f4e007de2531af22f6e834e84f1
mairi tikk <mairi.tikk@gmail.com>
2024-09-24 11:54:08 +0200

    draggable

--- rts/src/events/EventComponent.tsx
+++ rts/src/events/EventComponent.tsx
@@ -2,10 +2,15 @@ const EventComponent: React.FC = () => {
 
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         console.log(event)
-    }
+    };
+
+    const onDragStart = () => {
+        console.log('event')
+    };
     return (
         <div>
             <input onChange={onChange} type="text" />
+            <div draggable onDragStart={onDragStart}>Drag Me!</div>
         </div>
     );
 };
